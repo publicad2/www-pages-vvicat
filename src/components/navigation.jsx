6edded20b315b9cfbef6422705216f8c0b187e82
ygdzzz <ygdzzz@163.com>
2024-08-26 12:42:32 +0800

    logox

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -21,7 +21,7 @@ export const Navigation = (props) => {
           <a className="navbar-brand page-scroll" href="#page-top">
             {/*React Landing Page*/}
             {/*Home*/}
-            <img src={logo} alt="logo" style={{ width: '50px', height: 'auto' }}/>
+            <img src={logo} alt="logo" style={{ width: '100', height: 'auto' }}/>
           </a>{" "}
         </div>
 
